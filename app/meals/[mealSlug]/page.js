f0ab1ec2d834fd4e2cd7199b70dbd346f6765d9c
María Pía Nattero <mariapianattero@gmail.com>
2024-09-23 12:16:53 +0200

    FEAT: add details for meal detail page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,39 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+import classes from './page.module.css'
+import { getMeal } from '@/lib/meals';
+
 export default function MealDetailsPage( {params} ) {
+  const meal = getMeal(params.mealSlug)
+
+  if (!meal) {
+    notFound()
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
+      <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
       <main>
-        <h1>Meal Post</h1>
-        <p>{params.mealSlug}</p>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}></p>
       </main>
+      </>
     );
   }
